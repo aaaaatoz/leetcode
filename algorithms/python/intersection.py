334a601a2568c66acc7d16cfd8b12b4f8f456924
rafaxuskyfii <rafa.xu@skyfii.com>
2016-09-08 13:49:28 +0930

    intersection

---
+++ algorithms/python/intersection.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1).intersection(set(nums2)))
+
+    def intersectionNew(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return [x for x in set(nums1).intersection(set(nums2))]
